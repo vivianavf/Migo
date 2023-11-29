de76fe48aeca9419907df9f39e10f562b7bd2ed1
Viviana Vera Falconí <vivianavera03@gmail.com>
2023-11-29 16:14:52 -0500

    logica vehiculos

---
+++ migo/src/app/interfaces/vehiculo.ts
@@ -0,0 +1,16 @@
+export interface Vehiculo {
+    "telefono_conductor": number,
+    "placa": string,
+    "anio": number,
+    "categoria_vehiculo": number,
+    "color_vehiculo": number,
+    "imagen_izq": string,
+    "imagen_der": string,
+    "imagen_frontal": string,
+    "imagen_trasera": string,
+    "imagen_techo": string,
+    "estado": number,
+    "id_cliente": number,
+    "id_marca": number,
+    "id_modelo": number
+}
--- migo/src/app/providers/vehiculo.service.ts
+++ migo/src/app/providers/vehiculo.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Vehiculo } from '../interfaces/vehiculo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculoService {
 
-  constructor() { }
+  private baseURL = 'https://migoadvs.pythonanywhere.com/Database/Database/vehiculos/';
+  private formato = '?format=json'
+  private vehiculo!: Vehiculo;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getVehiculos():Observable<Vehiculo[]>{
+    return this.http.get<Vehiculo[]>(this.baseURL+this.formato);
+  }
+
+  getVehiculobyId(id: number):Observable<Vehiculo[]>{
+    return this.http.get<Vehiculo[]>(this.baseURL+id+"/"+this.formato);
+  }
+
+  crearVehiculo(vehiculo: Vehiculo): Observable<any>{
+    return this.http.post(this.baseURL, vehiculo);
+  }
+
+  setVehiculoActual(vehiculo: Vehiculo){
+    this.vehiculo = vehiculo;
+  }
+
+  getVehiculoActual(){
+    return this.vehiculo;
+  }
 }
